9f8433f7e34a3e3bfa07f6b023af57089a07b5e4
Sofia Montealegre <113529205+asmontealegre@users.noreply.github.com>
2023-01-26 02:03:42 -0500

    Día_2

--- habitacion/admin.py
+++ habitacion/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Habitacion
+from .models import TipoHabitacion
 # Register your models here.
+
+admin.site.register(Habitacion)
+admin.site.register(TipoHabitacion)
--- habitacion/models.py
+++ habitacion/models.py
@@ -10,27 +10,31 @@ class TipoHabitacion(models.Model):
         INDIVIDUAL = 'Individual', ('Individual')
         DOBLEKING = 'Doble King', ('Doble King')
         ALPINA = 'Cabaña Alpina', ('Cabaña Alpina')     
-    nombreHabitacion = models.CharField(max_length=45, choices=NombreHabitacion.choices, verbose_name="Nombre Habitacion", null=True)
-    descripcion = models.CharField(max_length=200, verbose_name="Descripcion Habitacion")
+    nombreHabitacion = models.CharField(max_length=45, choices=NombreHabitacion.choices, verbose_name="Nombre de Habitación", null=True)
     capacidad = models.CharField(max_length=45, verbose_name="Capacidad")
-    valor = models.IntegerField(verbose_name="Valor Habitacion")
-    class Sede(models.TextChoices):
-        ESTACION = 'estacion', ('La Estación')
-        BOSQUE = 'bosque', ('El Bosque')
-    sede = models.CharField(max_length=8, choices=Sede.choices, default=Sede.ESTACION, verbose_name="Sede")
     cantidad = models.PositiveIntegerField('Cantidad o Stock', default = 1)
+    valor = models.IntegerField(verbose_name="Valor")
+    descripcion = models.TextField(max_length=200, verbose_name="Descripción")
     class Estado(models.TextChoices):
         ACTIVO = '1', ('Activo')
         INACTIVO = '0', ('Inactivo')
     estado = models.CharField( max_length=1, choices=Estado.choices, default=Estado.ACTIVO, verbose_name="Estado")
+    
     def __str__(self):
-        return "%s " % (self.nombreHabitacion)
+        return "TipoHabitación %s" % (self.nombreHabitacion)
+
+    class Meta:
+        verbose_name = 'TipoHabitacion'
+        verbose_name_plural = 'TipoHabitaciones'
 
 
 # Habitacion.
 class Habitacion(models.Model):
     id = models.AutoField(primary_key = True)
-    
+    class Sede(models.TextChoices):
+        ESTACION = 'estacion', ('La Estación')
+        BOSQUE = 'bosque', ('El Bosque')
+    sede = models.CharField(max_length=8, choices=Sede.choices,  verbose_name="Sede", null= True)
     numeroHabitacion = models.IntegerField( verbose_name="Numero de Habitacion")
     class Disponibilidad(models.TextChoices):
         DISPONIBLE = 'Disponible',('Disponible')
@@ -39,6 +43,12 @@ class Habitacion(models.Model):
     class Estado(models.TextChoices):
         ACTIVO = '1', ('Activo')
         INACTIVO = '0', ('Inactivo')
-    estado = models.BooleanField(max_length=1, choices=Estado.choices, default=Estado.ACTIVO, verbose_name="Estado")
+    TipoHabitacion_id = models.ForeignKey(TipoHabitacion, on_delete=models.CASCADE, verbose_name="Tipo de Habitación",null=True)
+
     def __str__(self):
         return "Habitación %s" % (self.numeroHabitacion)
+
+    class Meta:
+        verbose_name = 'Habitacion'
+        verbose_name_plural = 'Habitaciones'
+        ordering = ['numeroHabitacion']
